f2dae2e709ecb9effd724858a617193d2c866176
victorgichohi <victorgichohi48@gmail.com>
2015-11-06 08:45:34 +0300

    solution

---
+++ NextBigNumber.py
@@ -0,0 +1,9 @@
+# Make larger number
+# Solve the problem below using PYTHON
+# Given a number whose digits are unique, find the next larger number that can be formed with those digits.
+# For example: 241 will output 421, 27 will output 72 and 68734 will output 87643
+
+def LargerNumber(num):
+    big = "".join(sorted(str(num), reverse=True))
+    return big
+print (LargerNumber(68734))
