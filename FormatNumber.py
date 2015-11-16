e0d03b25ae27f4989772d7a675437e998fc1710f
victorgichohi <victorgichohi48@gmail.com>
2015-11-16 09:40:55 +0300

    solution

--- FormatNumber.py
+++ FormatNumber.py
@@ -13,8 +13,8 @@ def FormatComma(x):
     elif x < 1000:
         return str(x)
     #devide the number by 1000,then add a comma after three numbers,then add the modulus of x divide by 
-    #the %03d is just a style format which places a comma after every three digits
+    #the %03d is just a style format which places a comma after every three digits.
     else:
-        return FormatComma(x / 1000) + ',' + '%03d' % (x % 1000)
+        return FormatComma( x / 1000) + ',' + '%03d' % (x % 1000)
 
 print (FormatComma(1000000))
