09760b34fdbe5a4673bcb12512435c291a65f22c
victorgichohi <victorgichohi48@gmail.com>
2015-11-17 11:23:13 +0300

    scrabble solution

---
+++ scrabble.py
@@ -0,0 +1,29 @@
+#job
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+def scrabble_score(word):
+    total = 0
+    for letter in word:
+        for let,num in score.items():
+            if letter == let:
+               total += num
+    return (total)
+print(scrabble_score("words"))
+
+
+//one liner
+from functools import reduce
+def get_word_worth(str_):
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+         
+    return reduce((lambda x , y: x + y),[val for let,val in score.items() if let in list(str_)])
+
+print(get_word_worth("words"))
+
